d808775b37eef7ebe822d6121bfa6507eb060f19
Jiyeon31 <zhdzhddlx@gmail.com>
2022-03-24 00:59:48 -0400

    added helper functions to handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -42,15 +42,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
-});
-
+// get single post
 router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
@@ -84,10 +76,8 @@ router.get('/post/:id', (req, res) => {
         return;
       }
 
-      // serialize the data
       const post = dbPostData.get({ plain: true });
 
-      // pass data to template
       res.render('single-post', {
         post,
         loggedIn: req.session.loggedIn
@@ -99,4 +89,14 @@ router.get('/post/:id', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
+
