3c7c55553907810430a05f0f0549b6c43c457bfa
Jiyeon31 <zhdzhddlx@gmail.com>
2022-03-24 00:33:38 -0400

    conditionally render the login links

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({});
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -20,6 +19,10 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -28,8 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
-
-app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
