8a09ebab612ca8b405a7128fc12e04f1b0872041
Jiyeon31 <zhdzhddlx@gmail.com>
2022-03-23 23:23:20 -0400

    added seeds

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const sequelize = require('./config/connection');
 
-// turn on routes
-app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on connection to db and server
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
